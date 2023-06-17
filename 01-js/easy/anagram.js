fa5ee800f793093f1651399db542b5ed785fb58e
2000jz <royacesparks@gmail.com>
2023-06-17 22:49:06 +0530

    anajram in progress

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -8,7 +8,27 @@
 */
 
 function isAnagram(str1, str2) {
+  if(str1.length != str2.length){
+        return false;
+    }
+    str1 = str1.sort;
+    str2= str2.sort;
+    str1= str1.toLowerCase();
+    str2 = str2.toLowerCase();
+    // if(str1 === str2){
+    //     return true;
+    // } else {
+
+    //     return false;
+    // }
+    for(var i =0 ;i<str1.length;i++){
+      if(str1.charAt(i) != str2.charAt(i)) return false;
+      else return true;
+    }
+  
 
 }
+var x = isAnagram("hello","world");
+console.log(x);
 
 module.exports = isAnagram;
